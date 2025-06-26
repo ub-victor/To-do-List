4e1e05412aadd730ab8b42d332569cf83156ee74
ub-victor <victoireushindi371@gmail.com>
2025-06-26 11:14:30 +0200

    Initial commit

--- script.js
+++ script.js
@@ -4,6 +4,6 @@
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
 const todoInput = document.getElementById("todoInput");
-const todoList = document.getElementById('todoList')
-const todoCount = document.getElementById('todoCount')
-const addButton = document.querySelector('.btn')
+const todoList = document.getElementById('todoList');
+const todoCount = document.getElementById('todoCount');
+const addButton = document.querySelector('.btn');
