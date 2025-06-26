b4bcede40706bf17c78571a9b2f01a91c87305fe
ub-victor <victoireushindi371@gmail.com>
2025-06-26 11:03:12 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 // Retrieve todo from local storage or intialize an empty array
-
+// Json stands for JavaScript Object Notation and it's basically the way of format Js  that's easily read
+// parse Converts a JavaScript Object Notation (JSON) string into an object.
+let todo = JSON.parse(localStorage.getItem("todo")) 
