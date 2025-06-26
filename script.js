86b8828413cc270b7156af5d4ba7fa37553d7154
ub-victor <victoireushindi371@gmail.com>
2025-06-26 15:38:03 +0200

    Initial commit

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ function displayTasks(){
         p.innerHTML = `
                         <div class="todo-container">
                             <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? 'disabled' : ''}>
+                            <p></p>
                         </div>
         `      
     });
